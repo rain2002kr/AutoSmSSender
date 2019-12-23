8eb0c01005e6ff627bda2edaeabe8a5d495675fd
rain2002kr <58737802+rain2002kr@users.noreply.github.com>
2019-12-23 16:36:47 +0900

    SQL database Test is done

--- app/src/main/java/rain2002kr/techworld/autosmssender/Sub1Activity.java
+++ app/src/main/java/rain2002kr/techworld/autosmssender/Sub1Activity.java
@@ -1,6 +1,7 @@
 package rain2002kr.techworld.autosmssender;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -8,8 +9,12 @@ import androidx.recyclerview.widget.RecyclerView;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
 import android.os.Bundle;
 import android.os.Parcelable;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -19,6 +24,7 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.FrameLayout;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.gson.Gson;
@@ -32,15 +38,20 @@ import java.util.List;
 import rain2002kr.techworld.autosmssender.Tel_item.TelItem;
 
 public class Sub1Activity extends AppCompatActivity {
-    Button btEnter;
+    Button btEnter,btDelete;
     EditText edName, edPhone;
     RecyclerView recyclerView;
     recyclerAdpter adpter;
     SharedPreferences pref;
     SharedPreferences.Editor preEditor;
-    Gson gson;
-    String json;
+    Gson gson; String json;
     ArrayList<TelItem> itemss = new ArrayList<TelItem>();
+    SQLiteDatabase database;
+    String dbName = "custom";
+    String tableName = "telList";
+
+    String TAG = "sub1Activity";
+    TextView textLog;
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -60,7 +71,9 @@ public class Sub1Activity extends AppCompatActivity {
                 screenchange(MainActivity.class, 100);
                 break;
             case R.id.serch:
-                printToast("검색 메뉴 선택됨");
+                //printToast("검색 메뉴 선택됨");
+                tableName = "telList";
+                executeQuery(tableName);
                 break;
             default:
                 break;
@@ -84,6 +97,8 @@ public class Sub1Activity extends AppCompatActivity {
         super.onResume();
         edName.setText(pref.getString("name",edName.getText().toString()));
         edPhone.setText(pref.getString("phone",edPhone.getText().toString()));
+        executeQuery(tableName);
+
     }
 
     @Override
@@ -92,12 +107,17 @@ public class Sub1Activity extends AppCompatActivity {
         setContentView( R.layout.activity_sub1 );
         UserFindViewById();
         UserFunction();
+        createOrOpenDataBase(dbName);
+
+        createTable(tableName);
+
 
     }
 
     //TODO UserFindViewById
     public void UserFindViewById(){
         btEnter = (Button) findViewById(R.id.btEnter);
+        btDelete = (Button) findViewById(R.id.btDelete);
         edName = (EditText) findViewById(R.id.edName);
         edPhone = (EditText) findViewById(R.id.edPhone);
         recyclerView = (RecyclerView) findViewById(R.id.recyler);
@@ -106,6 +126,7 @@ public class Sub1Activity extends AppCompatActivity {
         recyclerView.setAdapter(adpter);
         pref = getSharedPreferences("pref", MODE_PRIVATE);
         preEditor = pref.edit();
+        textLog = (TextView) findViewById(R.id.textLog);
 
     }
     //TODO UserFunction
@@ -119,9 +140,156 @@ public class Sub1Activity extends AppCompatActivity {
                 adpter.addItem(new TelItem(R.drawable.star, name, phone));
                 itemss.add(new TelItem(R.drawable.star, name, phone));
                 adpter.notifyDataSetChanged();
+                insertData(tableName, new TelItem(R.drawable.star, name, phone));
+
+            }
+        });
+
+        btDelete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                delSeletedItem(tableName);
             }
         });
     }
+    //TODO SQLiteDataBase code
+    //1. Create or open DataBase the method
+    public void createOrOpenDataBase(String dbName){
+        DataBaseHelper helper = new DataBaseHelper(this, dbName, null, 2, tableName );
+        database = helper.getWritableDatabase();
+
+    }
+    //2. Create table
+    public void createTable(String tableName){
+        if(database != null) {
+            String sql = "CREATE TABLE IF NOT EXISTS "
+                    + tableName + "(_id integer PRIMARY KEY autoincrement, " +
+                    "image int, name text, phone text)";
+            database.execSQL(sql);
+            println("DataBase table 생성됨." + tableName);
+        } else {
+            println("DataBase 먼저 생성하세요");
+        }
+    }
+
+    //3. insert value into the table at DB
+    public void insertData(String tableName, int image, String name, String phone){
+        println("insertData 호출됨");
+        if (database != null) {
+            String sql = "INSERT INTO " + tableName + "(image, name, phone)" + "values(?, ?, ?)";
+            Object[] params = {image, name, phone};
+            database.execSQL(sql, params);
+            println("insertData 추가됨");
+        } else {
+            println("Database 생성하세요.");
+        }
+    }
+    public void insertData(String tableName, TelItem item){
+        int image = item.getResId();
+        String name = item.getName();
+        String phone = item.getPhone();
+        println("insertData 호출됨");
+        if (database != null) {
+            String sql = "INSERT INTO " + tableName + "(image, name, phone)" + "values(?, ?, ?)";
+            Object[] params = {image, name, phone};
+            database.execSQL(sql, params);
+            println("insertData 추가됨");
+        } else {
+            println("Database 생성하세요.");
+        }
+    }
+    //4. data serch and load
+    public void executeQuery(String tableName) {
+        println("executeQuery 호출됨");
+        try {
+            if (database != null) {
+                String sql = "select _id, image, name, phone from " + tableName;
+                Cursor cursor = database.rawQuery(sql, null);
+
+                println("레코드 개수 : " + cursor.getCount());
+
+                for (int i = 0; i < cursor.getCount(); i++) {
+                    cursor.moveToNext();
+                    int id = cursor.getInt(0);
+                    int image = cursor.getInt(1);
+                    String name = cursor.getString(2);
+                    String phone = cursor.getString(3);
+
+                    println("레코드 #: " + id  + " name : " + name + " , phone :" + phone );
+                    adpter.addItem(new TelItem(R.drawable.star, name, phone));
+                }
+                println("executeQuery 마무리됨");
+                cursor.close();
+            }
+        }catch (Exception e){e.printStackTrace();}
+    }
+
+    //5. Delete item
+    public void delSeletedItem(String tableName) {
+        println("delSeletedItem 호출됨");
+        if (database != null) {
+            int id = 0;
+            try {
+            String sql = "select _id from " + tableName;
+            Cursor cursor = database.rawQuery(sql, null);
+                for (int i = 0; i < cursor.getCount(); i++) {
+                    //cursor.moveToNext();
+                    cursor.moveToLast();
+                    id = cursor.getInt(0);
+                }
+                sql ="delete from " + tableName + " WHERE _id =" + id;
+
+            database.execSQL(sql);
+
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    //DataBaseHelper
+    class DataBaseHelper extends SQLiteOpenHelper {
+        String dbName, tableName;
+
+        public DataBaseHelper(@Nullable Context context, @Nullable String dbName, @Nullable SQLiteDatabase.CursorFactory factory, int version, String tableName) {
+            super(context, dbName, factory, version);
+            this.tableName = tableName;
+
+        }
+
+        public void onOpen(SQLiteDatabase dbName){
+            println("DataBaseHelper onOpen 호출됨.");
+        }
+        @Override
+        public void onCreate(SQLiteDatabase sqLiteDatabase) {
+            println("DataBaseHelper onCreate 호출됨.");
+            try {
+                String sql = "create table if not exists "
+                        + tableName + "(_id integer PRIMARY KEY autoincrement, " +
+                        "image int, name text, phone text)";
+                sqLiteDatabase.execSQL(sql);
+                println("DataBase table 생성됨." + tableName);
+            } catch (Exception e){e.printStackTrace();}
+        }
+
+        @Override
+        public void onUpgrade(SQLiteDatabase sqLiteDatabase, int oldVer, int newVer) {
+            println("DataBaseHelper onUpgrade 호출됨.");
+            println("DataBase old Version :" + oldVer);
+            println("DataBase new Version :" + newVer);
+            if (newVer > 1){
+                sqLiteDatabase.execSQL("DROP TABLE IF EXISTS " + tableName);
+                println("old table 삭제됨");
+                String sql = "CREATE TABLE IF NOT EXISTS " + tableName +
+                        "(_id integer PRIMARY KEY autoincrement, " +
+                        "image int, name text, phone text)";
+                sqLiteDatabase.execSQL(sql);
+                println("new table 생성됨");
+            }
+        }
+    }
+
     //TODO Adpater for recyclerView
     class recyclerAdpter extends RecyclerView.Adapter<recyclerAdpter.ViewHolder>{
         ArrayList<TelItem> items = new ArrayList<TelItem>();
@@ -184,6 +352,9 @@ public class Sub1Activity extends AppCompatActivity {
     }
     public void printToast(String data){
         Toast.makeText(getApplicationContext(),data, Toast.LENGTH_LONG).show();
+    }
+    public void println(String data){
+        textLog.append(data + "\n");
     }
 
 }
